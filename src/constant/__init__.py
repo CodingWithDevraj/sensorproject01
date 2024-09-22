a1c21abd81dcc3c49fa0c8b2ed57378ca93b1aba
Devraj Choudhary <devrajror366@gmail.com>
2024-09-23 00:50:24 +0530

    new commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os 
+
+AWS_S3_BUCKET_NAME = "Wafer-fault"
+MONOGO_DATABSE_NAME = "Sensor"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "Quality"
+MONGO_DB_URL = "https://github.com/CodingWithDevraj"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
