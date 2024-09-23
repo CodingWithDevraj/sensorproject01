0061be1d394fa6817e67eafc94512f7ec98340a9
Devraj Choudhary <devrajror366@gmail.com>
2024-09-23 10:52:16 +0530

    new Commit of MongoDB

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,12 +1,12 @@
 import os 
 
 AWS_S3_BUCKET_NAME = "Wafer-fault"
-MONOGO_DATABSE_NAME = "Sensor"
+MONOGO_DATABSE_NAME = "newDatabase"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "Quality"
-MONGO_DB_URL = "https://github.com/CodingWithDevraj"
+MONGO_DB_URL = "mongodb+srv://devraj:Q1J2RNnKUb2pcYT1@cluster0.ulndc.mongodb.net/?retryWrites=true&w=majority"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
---
+++ upload_data.py
@@ -0,0 +1,32 @@
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import numpy as np
+
+#Url
+
+uri = "mongodb+srv://devraj:Q1J2RNnKUb2pcYT1@cluster0.ulndc.mongodb.net/?retryWrites=true&w=majority"
+
+#create a new client
+client = MongoClient(uri)
+
+#Defining Database
+#Collection Name in MongoDB
+
+DATABASE_NAME = 'newDatabase'
+COLLECTION_NAME = 'waferfault'
+
+df = pd.read_csv("C:\Users\Lenovo\Desktop\sensorproject\notebooks\wafer_23012020_041211 (3).csv")
+
+df.head()
+
+# Droping useless Columns
+df = df.drop("Unnamed: 0",axis = 1)
+
+#Converting Json file for MongoDB
+import json
+json_record = list(json.loads(df.T.to_json()).values())
+
+#Uploading Data in MongoDB
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
